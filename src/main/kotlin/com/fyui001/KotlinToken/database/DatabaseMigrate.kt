413fa7eddf9545b8e41c277e15bcf900eb90e70f
fyui001 <yun.cod@gmail.com>
2021-03-28 16:49:22 +0900

    マイグレーション完成

---
+++ src/main/kotlin/com/fyui001/KotlinToken/database/DatabaseMigrate.kt
@@ -0,0 +1,18 @@
+package com.fyui001.KotlinToken.database
+
+import org.flywaydb.core.Flyway
+import javax.sound.sampled.Port
+
+fun runDatabaseMigrate(dbHost: String, dbUser: String, dbPassword: String, dbDatabase: String, dbPort: String) {
+    println("====== MIGRATION MODE ======")
+    val flyway = Flyway.configure()
+        .dataSource("jdbc:mysql://$dbHost:$dbPort/$dbDatabase", dbUser, dbPassword)
+        .load()
+    println("\n** Current Migration Status **")
+    flyway.info().all().forEach {
+        println("=> [V${it.version.version}][${it.state.displayName}] ${it.description}")
+    }
+    println("\nRunning incomplete migration above...")
+    flyway.migrate()
+    println("Migration done!")
+}
